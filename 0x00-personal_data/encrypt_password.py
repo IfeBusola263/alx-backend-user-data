bf899f63c6318ff914f8dfdd90cb24027d970207
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-08 18:30:24 +0000

    password encryption

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates password encryption.
+"""
+import bcrypt
+
+
+def hash_password(password):
+    """
+    This function returns a a salted, hashed password,
+    which is a byte string.
+    """
+    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
