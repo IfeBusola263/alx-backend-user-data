73b04ef37e01ee96edb86b4d56c6fa6a170bd1af
Busola <ifebusolaj95@gmail.com>
2024-02-15 12:51:51 +0000

    Documented the session class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,4 +6,8 @@ from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
+    """
+    This is a session authentication class which will carry out different
+    functions to help with persistence.
+    """
     pass
