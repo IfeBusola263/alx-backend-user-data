82069b638951f0a7bf9adc617bb64efba18ef7c1
Busola <ifebusolaj95@gmail.com>
2024-02-15 12:49:59 +0000

    implemented the session Class to start a session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,12 +18,18 @@ main_paths = [
     '/api/v1/unauthorized/',
     '/api/v1/forbidden/']
 
+# Get the authentication type
 if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+
 elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module is a session authentication module.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    pass
