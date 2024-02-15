871bd9d3e9665ceb17e479d2cadea91bc7761001
Busola <ifebusolaj95@gmail.com>
2024-02-15 13:04:22 +0000

    implemented the Session class mapping for each user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 This module is a session authentication module.
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -10,4 +11,17 @@ class SessionAuth(Auth):
     This is a session authentication class which will carry out different
     functions to help with persistence.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        This method creates a Session ID for a user with user id 'user_id'
+        Returns None if 'user_id' is None or it is not a string.
+        """
+
+        if not user_id or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.__class__.user_id_by_session_id[session_id] = user_id
+        return session_id
