639a5191ac3142557e4eca2c69991603d63e922a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-19 12:48:22 +0000

    Implemented the add_user method for persisting a new user record in the database

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from typing import TypeVar
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password) -> TypeVar('User'):
+        """
+        This method creates a user, saves it and returns the user
+        object.
+        """
+        user = User()
+        user.email = email
+        user.hashed_password = hashed_password
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
