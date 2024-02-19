70c0f5bff3ce0317429cbad0d053f397e989b826
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-19 21:20:09 +0000

    Updated the auth class to handle session termination

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,8 +5,9 @@ This is an authentication module.
 
 from bcrypt import hashpw, gensalt, checkpw
 from db import DB
-from typing import TypeVar
+from typing import TypeVar, Union
 from user import User
+from uuid import uuid4
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -16,6 +17,13 @@ def _hash_password(password: str) -> bytes:
     to encrypt the password 'password', and returns it as bytes.
     """
     return hashpw(bytes(password, 'utf-8'), gensalt())
+
+
+def _generate_uuid() -> str:
+    """
+    This method generates a uuid and returns it.
+    """
+    return str(uuid4())
 
 
 class Auth:
@@ -49,3 +57,38 @@ class Auth:
                 bytes(password, 'utf-8'), existing_user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        This method returns a session ID.
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(existing_user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(
+            self, session_id: str) -> Union[TypeVar('User'), None]:
+        """
+        This method gets a user through the session id and returns the
+        user object. If the session id is invalid, it returns None.
+        """
+
+        if session_id:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+                return user
+            except NoResultFound:
+                return None
+
+        return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        This method invalidates a users session_id.
+        """
+        self._db.update_user(user_id, session_id=None)
+        return None
