de8a274981872e0c3e6a2a8a9c0b40ea4fc31912
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-19 21:06:15 +0000

    Implmented login protocal and returned the session_id as cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask App for a user application.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -34,6 +34,24 @@ def users():
         }), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    This Method validates login and sends the session_id as cookie.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    log_stat = AUTH.valid_login(email, password)
+
+    if log_stat:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+
+    abort(401)
 
 
 if __name__ == "__main__":
