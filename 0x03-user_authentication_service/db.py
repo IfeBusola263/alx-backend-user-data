4a9b2aae2e683629ef3c49a9d273d26520c0679e
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-19 14:20:00 +0000

    Implemented the user information update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar
+from typing import TypeVar, Dict
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
@@ -20,8 +20,8 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
-        # self._engine = create_engine("sqlite:///a.db", echo=False)
+        # self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -48,7 +48,7 @@ class DB:
         session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs: Dict) -> TypeVar('User'):
         """
         This method finds a user based on the kwargs parameters passed, and
         returns the user.
@@ -64,3 +64,22 @@ class DB:
             return user
         except (NoResultFound, InvalidRequestError) as err:
             raise err
+
+    def update_user(self, user_id: int, **kwargs: Dict) -> None:
+        """
+        This method updates a user and returns None on success. If the
+        argument passed does not correspond in type, to the users's
+        attribute, a value error is raised.
+        """
+
+        user = self.find_user_by(id=user_id)
+
+        if user:
+            try:
+                for key in kwargs:
+                    setattr(user, key, kwargs[key])
+                session = self._session
+                session.commit()
+                return None
+            except ValueError as err:
+                raise err
