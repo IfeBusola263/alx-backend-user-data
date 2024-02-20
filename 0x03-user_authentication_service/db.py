414efe6613972ddcf9513fed36b3dccb45f5d348
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-20 12:06:39 +0000

    removed the debug option

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -20,8 +20,8 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
-        # self._engine = create_engine("sqlite:///a.db", echo=False)
+        # self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
